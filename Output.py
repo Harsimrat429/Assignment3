a7e5f9f39edc5ceca6429267bc934624aa18e82a
WaggishYogesh <97587863+WaggishYogesh@users.noreply.github.com>
2022-02-14 22:42:14 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,98 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q1.py ============================================================
+Question 1
+Please give an input- Yogesh
+Total occurances
+y-1
+o-1
+g-1
+e-1
+s-1
+h-1
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q2.py ============================================================
+Question 2
+Please enter Day- 27
+Please enter Month- 5
+Please enter Year- 2022
+Next Date is: 28/5/2022
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q3.py ============================================================
+Question 3
+Plz enter Y to enter data- Y
+Plz give an integer input- 35
+Plz enter Y to enter data- Y
+Plz give an integer input- 56
+Plz enter Y to enter data- Y
+Plz give an integer input- 70
+Plz enter Y to enter data- Y
+Plz give an integer input- 89
+Plz enter Y to enter data- Y
+Plz give an integer input- 115
+Plz enter Y to enter data- N
+[35, 56, 70, 89, 115]
+[(35, 1225), (56, 3136), (70, 4900), (89, 7921), (115, 13225)]
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q4.py ============================================================
+Question 4
+Enter Grade Points- 9
+Your Grade is A and Excellent performance 
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q5.py ============================================================
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q6.py ============================================================
+Question 6
+Please Enter SID of Student- 21104122
+Please enter the name of the Student- Yogesh
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21104121
+Please enter the name of the Student- Jashan
+If you want to enter more details press Y/N- N
+Part a
+21104122 is Yogesh
+21104121 is Jashan
+
+Part b
+value sorted dictionary is ['Jashan', 'Yogesh']
+
+Part c
+Keys sorted dictionary is [21104121, 21104122]
+
+Part D
+Please enter the student's SID whose detail you need- 21104122
+Name of the required student is Yogesh
+
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q7.py ============================================================
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 5
+Fibonnaci sequence with 5 terms
+0
+1
+1
+2
+3
+Average of total 5 terms of sequence is 1.4
+END
+
+============================================================= RESTART: C:/Users/Lenovo/Downloads/Q8.py ============================================================
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
